15cd5db5cf65be9e7adebca4b181d8b019a0df66
mauroarms <mauro.armas14@gmail.com>
2024-02-06 21:55:54 -0300

    pequeña correcion en las cards

--- src/components/CardProducto.jsx
+++ src/components/CardProducto.jsx
@@ -1,20 +1,21 @@
 import { Container } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import "../css/cardProducto.css"
-import { Link, NavLink } from 'react-router-dom';
+import "../css/cardProducto.css";
+import { Link, NavLink } from "react-router-dom";
 
-const CardProducto = ({imagen}) => {
+const CardProducto = ({ imagen }) => {
   return (
     <Card className="cardProducto">
-      <Card.Img
-        className="imgCard"
-        variant="top"
-        src={imagen}
-      />
+      <Card.Img className="imgCard" variant="top" src={imagen} />
       <Card.Body className="d-flex flex-column justify-content-between">
         <Container>
-          <Card.Title className="tituloProducto">Cappuchino</Card.Title>
+          <Card.Text className="mb-0 text-secondary">
+            Bebida Caliente
+          </Card.Text>
+
+          <Card.Title className="tituloProducto mb-3">Cappuchino</Card.Title>
+
           <Card.Text>
             <strong>
               Descripción: <br />
@@ -22,7 +23,7 @@ const CardProducto = ({imagen}) => {
             Espresso Doble con espuma y chocolate
           </Card.Text>
         </Container>
-        
+
         <Card.Footer className="footerProducto">
           <Card.Text className="my-auto">
             <strong>
@@ -30,7 +31,12 @@ const CardProducto = ({imagen}) => {
             </strong>
             $1200
           </Card.Text>
-          <Button className="btnPrincipal ms-auto my-auto" variant="success" as={Link} to="/producto">
+          <Button
+            className="btnPrincipal ms-auto my-auto"
+            variant="success"
+            as={Link}
+            to="/producto"
+          >
             Ver
           </Button>
         </Card.Footer>
